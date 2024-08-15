3154c4ec9f7cddc8e3736e03524044d70e5e6d25
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2024-08-14 23:18:12 -0600

    Creacion de layout y nothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,13 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
     return (
-        <>
-            <Typography variant='h1'>JournalPage</Typography>
-            <MailOutline />
-        </>
+        <JournalLayout>
+            {/* <Typography>Enim mollit cillum exercitation exercitation officia sint ad aliquip labore excepteur do ullamco quis.</Typography> */}
+            <NothingSelectedView />
+        </JournalLayout>
     )
 }

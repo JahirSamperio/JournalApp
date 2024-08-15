bf52b9a0407019f034b355ea3bc8d5564bb61fb8
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2024-08-15 11:38:41 -0600

    Agregando ImageGallery y noteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,28 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
+import { AddOutlined, MailOutline } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedView } from "../views"
+import { NoteView, NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
             {/* <Typography>Enim mollit cillum exercitation exercitation officia sint ad aliquip labore excepteur do ullamco quis.</Typography> */}
             <NothingSelectedView />
+            {/* <NoteView /> */}
+            <IconButton
+                size="large"
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.85},
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{fontSize: 30}} />
+            </IconButton>
         </JournalLayout>
     )
 }
+ 
